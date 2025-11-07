dda6490cd0d960c4cc1db82d75fc508d60ef08d9
Astghik <astx.kras@gmail.com>
2025-11-07 13:21:02 +0400

    change some files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Lumea />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/collections" element={<BedLinenList />} />
       </Routes>
