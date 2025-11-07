792dc901895e2c353dea9c1f2a735107c1377bf4
Astghik <astx.kras@gmail.com>
2025-11-07 13:16:28 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// derive a path basename that works for both full URL and plain path
-const publicUrl = process.env.PUBLIC_URL || '/';
-let basename = '/';
-try {
-  if (publicUrl) {
-    // if PUBLIC_URL is a full URL (starts with http), use its pathname
-    if (/^https?:\/\//i.test(publicUrl)) {
-      basename = new URL(publicUrl).pathname.replace(/\/$/, '') || '/';
-    } else {
-      // if PUBLIC_URL is already a path like '/Lumea'
-      basename = publicUrl.replace(/\/$/, '') || '/';
-    }
-  }
-} catch (e) {
-  // fallback to '/'
-  basename = '/';
-}
+import { HashRouter } from 'react-router-dom';
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
